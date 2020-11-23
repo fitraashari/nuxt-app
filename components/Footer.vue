2ecf0485e5ef9cd33cbb9607ea809f104550434d
fitraashari <fitra.drive@gmail.com>
2020-11-23 16:51:34 +0700

    init

---
+++ components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <div class="card bg-dark text-light rounded-0">
+        <div class="card-body d-flex justify-content-center p-2">
+          <span>Copyright 2020</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <div class="card bg-dark text-light rounded-0">
+        <div class="card-body">
+          <span class="h2">Halaman Home</span>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic nihil asperiores rerum, adipisci dicta deserunt corrupti ratione fugiat iusto dolores nulla assumenda cumque porro ipsa aut molestias id accusantium praesentium.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
